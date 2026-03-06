89107190b6c4892294486e0e044c78ca9cbfb50b
Jeremy Perez <jeremyperez1409@gmail.com>
2026-03-06 18:39:01 -0500

    Fixed for deployment

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -3,7 +3,7 @@ import axios from "axios";
 export const axiosInstance = axios.create({
   baseURL:
     import.meta.env.MODE === "development"
-      ? `${import.meta.env.VITE_API_URL}/api`
+      ? `http://localhost:5001/api`
       : "/api",
   withCredentials: true,
 });
